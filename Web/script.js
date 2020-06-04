6fc4e5e28d59e5f368e50a2742ded1c6758c1a20
otnet84 <66313085+otnet84@users.noreply.github.com>
2020-06-04 08:07:18 -0700

    Add files via upload

---
+++ Web/script.js
@@ -0,0 +1,10 @@
+function printInfo(){
+	var message = "Hello World";
+	var email = "otwumbe@gmail.com";
+	var id = "HNG-05330";
+	var name = "Oheneba Twum";
+	var language = "javascript";
+	var msg = alert(message+"\nThis is "+name+" with HNGi7 ID" +id+" and email"+email+" using "+language+" for stage 2 task");
+	
+}
+printInfo();
